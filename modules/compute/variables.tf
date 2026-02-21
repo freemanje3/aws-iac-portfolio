9aec9962e3606b0ff392b52213a1636aaafa58d0
freemanje3 <70922268+freemanje3@users.noreply.github.com>
2026-02-21 17:19:37 -0500

    fix: ensure variables.tf is tracked and correct bracket in maintenance.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -2,3 +2,7 @@ variable "storage_kms_key_arn" {
   description = "ARN of the KMS CMK for general storage"
   type        = string
 }
+variable "cloudwatch_kms_key_arn" {
+  description = "ARN of the KMS CMK strictly for CloudWatch Logs"
+  type        = string
+}
